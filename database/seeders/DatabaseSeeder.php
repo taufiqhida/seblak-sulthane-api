758945ac7451ad69d92c98123cce42922a7bb963
abdulaziz27 <doelziez212@gmail.com>
2025-01-13 11:47:38 +0700

    feat: POS Panel and Endpoints auth, products, categories, orders, discounts, reports

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,14 +4,28 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
+        \App\Models\User::factory(20)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@seblaksulthane.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+
         $this->call([
-            RoleSeeder::class,
-            UserSeeder::class,
+            CategorySeeder::class,
+            ProductSeeder::class,
+            DiscountSeeder::class,
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,48 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/register', [AuthController::class, 'register']);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
-
-Route::middleware('auth:sanctum')->group(function () {
-    // Public Course Routes
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/profile', [UserController::class, 'profile']);
-    // Route::put('/profile', [UserController::class, 'updateProfile']);
-    Route::post('/profile/update', [UserController::class, 'updateProfile']);
-    Route::delete('/profile', [UserController::class, 'destroy']);
-
-
-    // Admin and Teacher Routes
-    Route::middleware(['role:owner|manager_cabang'])->group(function () {});
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
+
+//login api
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+//logout api
+Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+//products api
+Route::get('/products', [App\Http\Controllers\Api\ProductController::class, 'index'])->middleware('auth:sanctum');
+Route::post('/products', [App\Http\Controllers\Api\ProductController::class, 'store'])->middleware('auth:sanctum');
+Route::post('/products/edit', [App\Http\Controllers\Api\ProductController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/products/{id}', [App\Http\Controllers\Api\ProductController::class, 'destroy'])->middleware('auth:sanctum');
+//categories api
+Route::apiResource('/api-categories', App\Http\Controllers\Api\CategoryController::class)->middleware('auth:sanctum');
+
+//orders api
+Route::post('/save-order', [App\Http\Controllers\Api\OrderController::class, 'saveOrder'])->middleware('auth:sanctum');
+
+//discounts api
+Route::get('/api-discounts', [App\Http\Controllers\Api\DiscountController::class, 'index'])->middleware('auth:sanctum');
+
+Route::post('/api-discounts', [App\Http\Controllers\Api\DiscountController::class, 'store'])->middleware('auth:sanctum');
+
+// api resource report
+
+Route::get('/orders/{date?}', [App\Http\Controllers\Api\OrderController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/summary/{date?}', [App\Http\Controllers\Api\OrderController::class, 'summary'])->middleware('auth:sanctum');
+Route::get('/order-item/{date?}', [App\Http\Controllers\Api\OrderItemController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/order-sales', [App\Http\Controllers\Api\OrderItemController::class, 'orderSales'])->middleware('auth:sanctum');
